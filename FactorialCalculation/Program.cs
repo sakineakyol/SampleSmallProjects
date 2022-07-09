1a9e5cba269df28791b9dab4d47dd5d9dcec29e2
SAKINE\SAKINE <sakineeakyoll@gmail.com>
2022-07-09 21:13:51 +0300

    The algorithm calculates the factorial of a number received from the user and prints it on the screen.

--- FactorialCalculation/Program.cs
+++ FactorialCalculation/Program.cs
@@ -4,6 +4,15 @@ namespace FactorialCalculation
 {
     class Program
     {
+        //The algorithm calculates the factorial of a number received from the user and prints it on the screen.
+        /*
+             * 1. Step:  Start
+             * 2. Step: Get number value from user
+             * 3. Step: If waterTemp is >=100, write on screen ("Water is in Gaseous State")
+             *          Otherwise, if waterTemp <=0, write to screen ("Water is in Solid")
+             *          Otherwise, write to screen ("Water is in Liquid")
+             * 4. Step: End
+             */
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
