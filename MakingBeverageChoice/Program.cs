8bc7a372304470cc5d8d9c2564e84205c3a136c3
SAKINE\SAKINE <sakineeakyoll@gmail.com>
2022-07-11 00:53:22 +0300

    A program that prints which beverage someone prefers according to the preference information received from the user. (Tea: T - Lemonade: L - Soda: S - Cola: C)

---
+++ MakingBeverageChoice/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace MakingBeverageChoice
+{
+    class Program
+    {
+        //A program that prints which beverage someone prefers according to the preference information received from the user. (Tea: T - Lemonade: L - Soda: S - Cola: C)
+        static void Main(string[] args)
+        {
+        preferAgain: Console.WriteLine("Which drink do you prefer? \n Tea: T - Lemonade: L - Soda: S - Cola: C ");
+
+            string prefer = Console.ReadLine();
+            #region IF- ELSE
+
+            if (prefer == "T" || prefer == "t")
+            {
+                Console.WriteLine("Your beverage of choice was tea.");
+            }
+            else if (prefer == "L" || prefer == "l")
+            {
+                Console.WriteLine("Your beverage of choice was lemonade.");
+            }
+            else if (prefer == "S" || prefer == "s")
+            {
+                Console.WriteLine("Your beverage of choice was soda.");
+            }
+            else if (prefer == "C" || prefer == "c")
+            {
+                Console.WriteLine("Your beverage of choice was cola.");
+            }
+            else
+            {
+                Console.WriteLine("You chose the wrong drink. Please try again.");
+                goto preferAgain;
+            }
+            #endregion
+
+            #region SWITCH - CASE
+            //No comparisons are made in the switch case. Only variable is written. In each of the cases, the values to be controlled are written in that variable.
+            switch (prefer)
+            {
+                case "T":
+                case "t":
+                    Console.WriteLine("Your beverage of choice was tea.");
+                    break;
+                case "L":
+                case "l":
+                    Console.WriteLine("Your beverage of choice was lemonade.");
+                    break;
+                case "S":
+                case "s":
+                    Console.WriteLine("Your beverage of choice was soda.");
+                    break;
+                case "C":
+                case "c":
+                    Console.WriteLine("Your beverage of choice was cola.");
+                    break;
+                default:
+                    Console.WriteLine("You chose the wrong drink. Please try again.");
+                    goto preferAgain;
+
+            }
+            #endregion
+        }
+    }
+}
---
+++ OperationsWithTwoNumbers/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace OperationsWithTwoNumbers
+{
+    class Program
+    {
+        //A program that finds the sums of two numbers if any of the 2 integers received from the user is negative, their product if both are negative, and the arithmetic averages if both are positive. If 0 is entered, whatever number 0 was entered should be requested again.
+        static void Main(string[] args)
+        {
+
+        numberAgain: Console.WriteLine("Please enter the first number.");
+            int number1 = int.Parse(Console.ReadLine());
+
+            if (number1 == 0)
+            {
+                Console.WriteLine("You cannot enter the number 0 in this application. Please re-enter the 1st number:");
+                goto numberAgain;
+            }
+
+        numberAgain2: Console.WriteLine("Please enter the second number.");
+            int number2 = int.Parse(Console.ReadLine());
+
+            if (number2 == 0)
+            {
+                Console.WriteLine("You cannot enter the number 0 in this application. Please re-enter the 2nd number:");
+                goto numberAgain2;
+            }
+
+
+
+            if (number1 < 0 || number2 < 0)
+            {
+                Console.WriteLine("Since one of the numbers is entered as negative, their total is:" + (number1 + number2));
+            }
+            else if (number1 < 0 && number2 < 0)
+            {
+                Console.WriteLine("Since both numbers are negative, the product of two numbers is:" + (number1 * number2));
+            }
+            else if (number1 > 0 && number2 > 0)
+            {
+                Console.WriteLine("Since two positives are entered, the arithmetic mean of the numbers is::" + ((number1 + number2) / 2));
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
