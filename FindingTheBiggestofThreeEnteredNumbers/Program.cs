7691b29ced009208666605cb77e06fb04a2e1719
SAKINE\SAKINE <sakineeakyoll@gmail.com>
2022-07-10 22:18:58 +0300

    A program that finds the sums of two numbers if any of the 2 integers received from the user is negative, their product if both are negative, and the arithmetic averages if both are positive. If 0 is entered, whatever number 0 was entered should be requested again.

---
+++ FindingTheBiggestofThreeEnteredNumbers/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace FindingTheBiggestofThreeEnteredNumbers
+{
+    class Program
+    {
+        //A program that finds the biggest of three numbers entered before or after the three numbers received from the user, and prints it on the screen.
+
+        /*
+           * 1. Step:  Start
+           * 2. Step: Get number1,number2 and number3 value from user
+           * 3. Step: If number1 > number2 and number1 > number3 then Write to Screen("Number1 is the biggest number.)
+            *           Otherwise, If number2 > number1 and number2 > number3, Write to Display("Number2 is the biggest number.)
+            *           Otherwise, If number3 > number1 and number3 > number2, Write to Display("Number is the biggest number.)
+            *           Otherwise, Write to Screen("Numbers Equal")
+           * 4. Step: End
+           */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the first number.");
+            double number1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Please enter the second number.");
+            double number2 = Convert.ToDouble(Console.ReadLine());
+
+
+            Console.WriteLine("Please enter the third number.");
+            double number3 = Convert.ToDouble(Console.ReadLine());
+
+            double biggest = number1;
+
+            if (number2 > biggest)
+            {
+                biggest = number2;
+                if (number3 > biggest)
+                {
+                    biggest = number3;
+                }
+            }
+            else
+            {
+                if (number3 > biggest)
+                {
+                    biggest = number3;
+                }
+            }
+
+            Console.WriteLine("The biggest of the numbers you entered: " + biggest);
+            Console.ReadLine();
+
+        }
+    }
+}
